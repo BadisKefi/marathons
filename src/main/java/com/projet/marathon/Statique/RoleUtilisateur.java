d043bc2459312e3f6ca2419679fd3c20fc50a3b7
badiskefi <badiskefi@gmail.com>
2023-05-23 11:26:08 +0100

    this is another project

---
+++ src/main/java/com/projet/marathon/Statique/RoleUtilisateur.java
@@ -0,0 +1,7 @@
+package com.projet.marathon.Statique;
+
+public class RoleUtilisateur {
+    public static int id;
+    public static String role;
+
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,18 @@
+module com.projet.marathon {
+    requires javafx.controls;
+    requires javafx.fxml;
+    /*requires mysql.connector.java;*/
+    requires java.sql;
+    requires java.mail;
+
+    opens com.projet.marathon to javafx.fxml;
+    exports com.projet.marathon;
+    exports com.projet.marathon.controllers;
+    opens com.projet.marathon.controllers to javafx.fxml;
+    exports com.projet.marathon.entities;
+    opens com.projet.marathon.entities to javafx.fxml;
+
+    exports com.projet.marathon.Statique;
+    opens com.projet.marathon.Statique to javafx.fxml;
+
+}
